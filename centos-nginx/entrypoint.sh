7316eb69f715ed4164316b383ed45ea04dffb689
王于晟 <brande.wang@gihtg.com>
2020-06-03 09:03:31 +0800

    update centos-nginx

--- centos-nginx/entrypoint.sh
+++ centos-nginx/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/sbin/crond
-echo '10 0 * * * /bin/sh /log_archive.sh' >> /var/spool/cron/root
+echo '10 0 * * * /bin/sh /log_archive.sh' > /var/spool/cron/root
 /usr/bin/supervisord -c /etc/supervisord.conf
